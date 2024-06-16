e88b197a3d000c5f2a89172892a987c881874cf8
jungjihun <jungjihun@unfine.co.kr>
2024-06-16 21:19:40 +0900

    로드밸런싱 테스트

--- first-service/src/main/java/com/example/firstservice/sample/controller/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/sample/controller/FirstServiceController.java
@@ -1,6 +1,8 @@
 package com.example.firstservice.sample.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/first-service")
 public class FirstServiceController {
+
+    private final Environment env;
+
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -23,7 +31,12 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "hi, there. this is a message from first service";
+    public String check(HttpServletRequest request) {
+        /*
+        1개의 서비스를 다중 포트로 여러 개의 앱을 띄워 로드밸런싱을 테스트하였다.
+        spring cloud gateway의 로드밸런싱 기본 알고리즘은 라운드로빈 방식이다.
+         */
+        log.info("Server port={}", request.getServerPort());
+        return String.format("hi, there. this is a message from first service on PORT %s", env.getProperty("local.server.port"));
     }
 }
