dcb66cfdaea5150ae3f88a14a75b028411337776
jungjihun <jungjihun@unfine.co.kr>
2024-07-01 23:27:01 +0900

    DTO deserialize 설정

--- user-service/src/main/java/com/example/userservice/user/vo/ResponseUser.java
+++ user-service/src/main/java/com/example/userservice/user/vo/ResponseUser.java
@@ -1,12 +1,14 @@
 package com.example.userservice.user.vo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 @Getter
 @Setter
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseUser {
 
     private String userId;
