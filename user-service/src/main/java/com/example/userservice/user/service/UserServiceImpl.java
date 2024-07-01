6bf5a475f84c52f45e050c2b51f12e7eaa5f840a
jungjihun <jungjihun@unfine.co.kr>
2024-07-01 23:26:21 +0900

    modelmapper bean 추가

--- user-service/src/main/java/com/example/userservice/user/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/user/service/UserServiceImpl.java
@@ -5,7 +5,6 @@ import com.example.userservice.user.dto.UserDto;
 import com.example.userservice.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -19,12 +18,11 @@ public class UserServiceImpl implements UserService {
 
     private final BCryptPasswordEncoder passwordEncoder;
 
+    private final ModelMapper modelMapper;
+
     @Override
     public UserDto createUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
-
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
         UserEntity userEntity = modelMapper.map(userDto, UserEntity.class);
         userEntity.setEncryptedPwd(passwordEncoder.encode(userDto.getPwd()));
