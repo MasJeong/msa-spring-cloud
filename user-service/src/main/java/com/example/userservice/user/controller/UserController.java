bd7d48c480a4d0386f6236c16ac82f5a0dfa6d04
jungjihun <jungjihun@unfine.co.kr>
2024-07-01 23:27:20 +0900

    url 변경 및 상태 체크 api 추가

--- user-service/src/main/java/com/example/userservice/user/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/user/controller/UserController.java
@@ -7,27 +7,26 @@ import com.example.userservice.user.vo.ResponseUser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/user-service")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
+    private final Environment environment;
+
+    private final ModelMapper modelMapper;
+
     @PostMapping
     public ResponseEntity<Object> createUser(@RequestBody RequestUser requestUser) {
 
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         UserDto userDto = modelMapper.map(requestUser, UserDto.class);
 
         userService.createUser(userDto);
@@ -37,4 +36,8 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
     }
 
+    @GetMapping(value = "/health-check")
+    public String healthCheck() {
+        return String.format("hi, %s", environment.getProperty("local.server.port"));
+    }
 }
