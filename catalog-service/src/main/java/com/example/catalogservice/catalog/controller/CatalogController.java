ac06f650566810c852e643918e7ee5673255ae5d
jeongjihun <jihun3654@naver.com>
2024-07-18 20:28:35 +0900

    테스트 소스 제거

--- catalog-service/src/main/java/com/example/catalogservice/catalog/controller/CatalogController.java
+++ catalog-service/src/main/java/com/example/catalogservice/catalog/controller/CatalogController.java
@@ -35,7 +35,6 @@ public class CatalogController {
         List<CatalogEntity> catalogList = catalogService.getAllCatalogs();
         List<ResponseCatalog> resCatalogs = new ArrayList<>();
 
-        System.out.println("test");
         catalogList.forEach(catalog -> resCatalogs.add(modelMapper.map(catalog, ResponseCatalog.class)));
 
         return ResponseEntity.status(HttpStatus.OK).body(resCatalogs);
