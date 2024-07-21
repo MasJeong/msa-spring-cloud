8194a92d83082736864ae62cb297fda13b7fc97d
jeongjihun <jihun3654@naver.com>
2024-07-21 18:57:25 +0900

    findByEmail 추가

--- user-service/src/main/java/com/example/userservice/user/repository/UserRepository.java
+++ user-service/src/main/java/com/example/userservice/user/repository/UserRepository.java
@@ -3,7 +3,11 @@ package com.example.userservice.user.repository;
 import com.example.userservice.user.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     UserEntity findByUserId(String userId);
+
+    Optional<UserEntity> findByEmail(String email);
 }
