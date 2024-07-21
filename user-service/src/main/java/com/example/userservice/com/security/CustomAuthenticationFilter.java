4d565d73cb647a19d14162acf1d834fa217fc075
jeongjihun <jihun3654@naver.com>
2024-07-21 18:58:13 +0900

    security 설정 작업중

--- user-service/src/main/java/com/example/userservice/com/security/CustomAuthenticationFilter.java
+++ user-service/src/main/java/com/example/userservice/com/security/CustomAuthenticationFilter.java
@@ -1,26 +1,42 @@
 package com.example.userservice.com.security;
 
+import com.example.userservice.user.dto.UserDto;
 import com.example.userservice.user.service.UserService;
 import com.example.userservice.user.vo.RequestLogin;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.crypto.SecretKey;
 import java.io.IOException;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Date;
 
+@RequiredArgsConstructor
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-    public CustomAuthenticationFilter(AuthenticationManager authenticationManager, UserService userService, Environment env) {
+    private final UserService userService;
 
+    private final Environment env;
+
+    public CustomAuthenticationFilter(AuthenticationManager authenticationManager,
+                                   UserService userService, Environment env) {
+        super(authenticationManager);
+        this.userService = userService;
+        this.env = env;
     }
 
     /**
@@ -38,7 +54,6 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
             RequestLogin credential = new ObjectMapper().readValue(request.getInputStream(), RequestLogin.class);
 
             /*
-            getAuthenticationManager: 인증을 처리하는 메서드
             authenticate: 사용자 인증을 수행
             UsernamePasswordAuthenticationToken: 사용자의 인증 정보를 담는 객체
              */
@@ -55,12 +70,32 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
     }
 
     @Override
-    protected void successfulAuthentication(HttpServletRequest request,
-                                            HttpServletResponse response,
-                                            FilterChain chain,
-                                            Authentication authResult) throws IOException, ServletException {
+    protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain,
+                                            Authentication auth) {
 
+        String userName = ((User) auth.getPrincipal()).getUsername();
+        UserDto userDetails = userService.getUserDetailsByEmail(userName);
 
-//        super.successfulAuthentication(request, response, chain, authResult);
+        String secret = env.getProperty("token.secret");
+        assert secret != null;
+
+        byte[] secretKeyBytes = Base64.getEncoder().encode(secret.getBytes());
+
+        SecretKey secretKey = Keys.hmacShaKeyFor(secretKeyBytes);
+
+        Instant now = Instant.now();
+
+        String expireTime = env.getProperty("token.expiration_time");
+        assert expireTime != null;
+
+        String token = Jwts.builder()
+                .subject(userDetails.getUserId())
+                .expiration(Date.from(now.plusMillis(Long.parseLong(expireTime))))
+                .issuedAt(Date.from(now))
+                .signWith(secretKey)
+                .compact();
+
+        res.addHeader("token", token);
+        res.addHeader("userId", userDetails.getUserId());
     }
 }
--- user-service/src/main/java/com/example/userservice/com/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/com/security/WebSecurity.java
@@ -2,32 +2,47 @@ package com.example.userservice.com.security;
 
 import com.example.userservice.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.Arrays;
+
+@Slf4j
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurity {
 
+//    private final AuthenticationConfiguration authenticationConfiguration;
+
     private final UserService userService;
 
-    private final AuthenticationConfiguration authenticationConfiguration;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     private final Environment env;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // authenticationManager 객체 생성
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(passwordEncoder);
+
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/users/**").permitAll()
@@ -36,25 +51,60 @@ public class WebSecurity {
                         .requestMatchers("/error/**").permitAll()
                         .requestMatchers("/**").access((authentication, request) -> {
                             String clientIp = request.getRequest().getRemoteAddr();
-                            return new AuthorizationDecision(clientIp.equals("127.0.0.1"));
+                            log.debug("client ip is = {}", clientIp);
+
+                            // 허용된 IP 리스트
+                            String[] allowedIps = {"192.168.35.194", "192.168.35.195", "192.168.35.196"};
+
+                            // IP가 허용된 리스트에 포함되어 있는지 확인
+                            boolean isAllowed = Arrays.asList(allowedIps).contains(clientIp);
+
+                            return new AuthorizationDecision(isAllowed);
                         })
                 )
-                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
-                .addFilter(getAuthenticationFilter(authenticationConfiguration));
+                .authenticationManager(authenticationManager)
+                .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 사용자 세션 저장하지 않음
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)) // 응답 헤더에 X-Frame-Options 추가 - 클릭재킹 공격 방어 - 동일 출처만 <iframe> 로드 가능.
+                .addFilter(customAuthenticationFilter(authenticationManager));
 
         return http.build();
     }
 
+//    /**
+//     * customAuthenticationFilter bean 생성
+//     * @return customAuthenticationFilter bean
+//     */
+//    @Bean
+//    public CustomAuthenticationFilter customAuthenticationFilter() throws Exception {
+//        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter();
+//        customAuthenticationFilter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
+//
+//        return customAuthenticationFilter;
+//    }
 
-    private CustomAuthenticationFilter getAuthenticationFilter(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return new CustomAuthenticationFilter(authenticationManager(authenticationConfiguration), userService, env);
-    }
-
+    /**
+     * AuthenticationManager 설정
+     * @param authenticationConfiguration security 인증 설정 관리 객체
+     * @return AuthenticationManager bean
+     */
     @Bean
-    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
+    /**
+     * customAuthenticationFilter bean 생성
+     * @return customAuthenticationFilter bean
+     */
+    @Bean
+    public CustomAuthenticationFilter customAuthenticationFilter(AuthenticationManager authenticationManager) {
+        return new CustomAuthenticationFilter(authenticationManager, userService, env);
+    }
+
+    /**
+     * 비밀번호 암호화 모듈
+     * @return 암호화 bean
+     */
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
