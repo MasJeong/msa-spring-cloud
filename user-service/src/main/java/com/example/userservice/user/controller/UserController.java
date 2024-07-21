1e4323e121727a07a9a054984c18d502fc3c5527
jeongjihun <jihun3654@naver.com>
2024-07-21 18:57:35 +0900

    url 변경

--- user-service/src/main/java/com/example/userservice/user/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/user/controller/UserController.java
@@ -19,7 +19,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user-service")
+@RequestMapping("/")
 public class UserController {
 
     private final UserService userService;
