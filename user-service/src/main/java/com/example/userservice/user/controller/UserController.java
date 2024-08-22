ea6deba156290bc7b3870ea389afbc4a3bf36756
jeongjihun <jihun3654@naver.com>
2024-08-22 12:53:14 +0900

    feat: spring cloud config, bootstrap 디펜던시 추가

--- user-service/src/main/java/com/example/userservice/user/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/user/controller/UserController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +26,22 @@ public class UserController {
     private final UserService userService;
 
     private final ModelMapper modelMapper;
+
+    private final Environment env;
+
+    /**
+     * 사용자 전체 목록 조회
+     * @return 사용자 목록
+     */
+    @GetMapping
+    public String healthCheck() {
+        return String.format("It's working is User Service"
+                + ", port(local.server.port)=" + env.getProperty("local.server.port")
+                + ", port(server.port)=" + env.getProperty("server.port")
+                + ", token secret=" + env.getProperty("token.secret")
+                + ", token expiration time=" + env.getProperty("token.expiration-time")
+        );
+    }
 
     /**
      * 사용자 전체 목록 조회
