c91e1bba775c594c52e58583f8afd529171aae51
jeongjihun <jihun3654@naver.com>
2024-10-19 13:56:53 +0900

    clear git cache

--- catalog-service/src/main/java/com/example/catalogservice/catalog/repository/CatalogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.catalogservice.catalog.repository;
-
-import com.example.catalogservice.catalog.domain.CatalogEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CatalogRepository extends JpaRepository<CatalogEntity, Long> {
-
-    CatalogEntity findByProductId(String productId);
-}
