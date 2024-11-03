a6ce180227f746e773d2558c9910099ee1780784
jeongjihun <jihun3654@naver.com>
2024-11-03 22:21:11 +0900

    chore: RestTemplate 로드밸런싱 설정 추가

--- user-service/src/main/java/com/example/userservice/com/config/RestTemplateConfig.java
+++ user-service/src/main/java/com/example/userservice/com/config/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package com.example.userservice.com.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,9 +10,12 @@ public class RestTemplateConfig {
 
     /**
      * RestTemplate Bean 생성
+     * - @LoadBalanced : Spring Cloud에서 제공하는 로드밸런싱 기능으로 Eureka 등의 서비스 등록 및 검색 기능과 같이 사용되며
+     *                   default 알고리즘은 라운드 로빈이다.
      * @return RestTemplate Bean
      */
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
