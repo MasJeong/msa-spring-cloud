a1ea9045a14ade370f841f06b1544ee75ea3ae9f
jeongjihun <jihun3654@naver.com>
2024-12-23 11:36:07 +0900

    chore(테스트): 테스트 환경 설정 및 jwt 생성 및 파싱 테스트 추가

---
+++ user-service/src/test/java/com/example/userservice/com/security/JwtTest.java
@@ -0,0 +1,74 @@
+package com.example.userservice.com.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.Base64;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@Slf4j
+@ActiveProfiles({"test"})
+@SpringBootTest
+class JwtTest {
+
+    @Autowired
+    private Environment env;
+
+    @Test
+    @DisplayName("JWT 생성 및 파싱")
+    void testCreateAndParseToken() {
+        // given
+        String username = "testuser1";
+        String secret = env.getProperty("token.secret");
+        assert secret != null;
+
+        log.debug("== secret: {}", secret);
+
+        // 비밀 키 생성
+        byte[] secretKeyBytes = Base64.getEncoder().encode(secret.getBytes(StandardCharsets.UTF_8));
+        SecretKey secretKey = Keys.hmacShaKeyFor(secretKeyBytes);
+
+        String expireTime = env.getProperty("token.expiration-time");
+        assert expireTime != null;
+
+        Instant now = Instant.now();
+
+        String roleName = "ROLE_ADMIN";
+
+        // Generate JWT
+        String token = Jwts.builder()
+                .subject(username)
+                .claim("ROLE", roleName)
+                .expiration(Date.from(now.plusMillis(Long.parseLong(expireTime))))  // 만료 시간
+                .issuedAt(Date.from(now))   // 발급 시간
+                .signWith(secretKey)        // 서명
+                .compact();
+
+        // when (JWT parse)
+        Claims payload = Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+        // then
+        assertNotNull(token);
+        assertEquals(payload.getSubject(), username);
+        assertEquals(payload.get("ROLE"), roleName);
+    }
+
+}
