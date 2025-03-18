2cf40f6e9f5755af1c06210cf301be5be0251b9d
jeongjihun <jihun3654@naver.com>
2025-03-18 19:49:19 +0900

    :recycle: JWT 관련 예외 처리 소스 추가함

--- user-service/src/main/java/com/example/userservice/com/security/CustomJwtValidationFilter.java
+++ user-service/src/main/java/com/example/userservice/com/security/CustomJwtValidationFilter.java
@@ -3,6 +3,7 @@ package com.example.userservice.com.security;
 import com.example.userservice.com.security.constants.JwtConstant;
 import com.google.common.net.HttpHeaders;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
@@ -19,6 +20,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.crypto.SecretKey;
@@ -29,6 +31,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+/**
+ * 사용자 정의 JWT 검증 Filter
+ */
 public class CustomJwtValidationFilter extends OncePerRequestFilter {
 
     private final Environment env;
@@ -47,43 +52,55 @@ public class CustomJwtValidationFilter extends OncePerRequestFilter {
 
         String header = request.getHeader(HttpHeaders.AUTHORIZATION);
 
-        if (header != null && header.startsWith(JwtConstant.TOKEN_PREFIX)) {
-            String token = header.substring(7);
-
-            try {
-                String secret = env.getProperty("token.secret");
-                Assert.notNull(secret, "secret should not be empty");
-
-                // 비밀 키 생성
-                byte[] secretKeyBytes = Base64.getEncoder().encode(secret.getBytes(StandardCharsets.UTF_8));
-                SecretKey secretKey = Keys.hmacShaKeyFor(secretKeyBytes);
-
-                Claims claims = Jwts.parser()
-                        .verifyWith(secretKey)
-                        .build()
-                        .parseSignedClaims(token)
-                        .getPayload();
-
-                List<?> roleObjects = claims.get("roles", List.class);
-
-                List<SimpleGrantedAuthority> authorities = roleObjects.stream()
-                        .filter(obj -> obj instanceof Map)  // LinkedHashMap 타입 확인
-                        .map(obj -> (Map<?, ?>) obj)
-                        .map(roleMap -> new SimpleGrantedAuthority((String) roleMap.get("authority")))
-                        .collect(Collectors.toList());
-
-                // 인증 객체 생성
-                UserDetails userDetails = userService.loadUserByUsername(claims.getSubject());
-                Authentication authentication = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, authorities
-                );
-
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            } catch (JwtException e) {
-                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid token");
-                return;
-            }
+        if(!StringUtils.hasText(header) || !header.startsWith(JwtConstant.TOKEN_PREFIX)) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid or missing Authorization header");
+            return;
         }
+
+        String token = header.substring(7);
+
+        try {
+            String secret = env.getProperty("token.secret");
+            Assert.notNull(secret, "secret should not be empty");
+
+            // 비밀 키 생성
+            byte[] secretKeyBytes = Base64.getEncoder().encode(secret.getBytes(StandardCharsets.UTF_8));
+            SecretKey secretKey = Keys.hmacShaKeyFor(secretKeyBytes);
+
+            Claims claims = Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+
+            List<?> roleObjects = claims.get("roles", List.class);
+
+            List<SimpleGrantedAuthority> authorities = roleObjects.stream()
+                    .filter(obj -> obj instanceof Map)  // LinkedHashMap 타입 확인
+                    .map(obj -> (Map<?, ?>) obj)
+                    .map(roleMap -> new SimpleGrantedAuthority((String) roleMap.get("authority")))
+                    .collect(Collectors.toList());
+
+            // 인증 객체 생성
+            UserDetails userDetails = userService.loadUserByUsername(claims.getSubject());
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    userDetails, null, authorities
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (ExpiredJwtException e) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token has expired");
+            return;
+        } catch (JwtException e) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid token");
+            return;
+        } catch (Exception e) {
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                    "An error occurred while processing the token");
+            return;
+        }
+
+
         chain.doFilter(request, response);
     }
 }
