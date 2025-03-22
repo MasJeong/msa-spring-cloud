372a3957c5768e72a781c82d99f4c7aa749f1786
jeongjihun <jihun3654@naver.com>
2025-03-22 13:22:24 +0900

    :safety_vest: 주문 요청 시 검증 메시지 수정

--- order-service/src/main/java/com/example/orderservice/order/vo/RequestOrder.java
+++ order-service/src/main/java/com/example/orderservice/order/vo/RequestOrder.java
@@ -2,23 +2,23 @@ package com.example.orderservice.order.vo;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RequestOrder {
 
-    @NotNull(message = "User ID cannot be null")
+    @NotNull(message = "Product ID cannot be null. ")
     private String productId;
 
-    @NotNull(message = "Name cannot be null")
-    @Size(min = 2, message = "Name not be less than two characters")
+    @NotNull(message = "Qty cannot be null")
+    @Size(min = 1, max = 11, message = "The Qty can be a minimum of 1 characters and a maximum of 11 characters. ")
     private Integer qty;
 
-    @NotNull(message = "Password cannot be null")
-    @Size(min = 8, message = "Password must be equal or greater than 8 characters")
+    @NotNull(message = "UnitPrice cannot be null")
+    @Size(min = 3, max = 11, message = "The unitPrice can be a minimum of 3 characters and a maximum of 11 characters. ")
     private Integer unitPrice;
 }
