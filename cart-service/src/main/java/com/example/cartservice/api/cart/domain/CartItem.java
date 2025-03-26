18521025aeca0391f8d23fb725e2ec99ddf4e70f
jeongjihun <jihun3654@naver.com>
2025-03-26 16:47:23 +0900

    feat: :sparkles: 장바구니 API 추가함
    - catalog-service에서 장바구니 api 가져옴

---
+++ cart-service/src/main/java/com/example/cartservice/api/cart/domain/CartItem.java
@@ -0,0 +1,24 @@
+package com.example.cartservice.api.cart.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItem {
+
+    /** 상품 ID */
+    private String productId;
+
+    /** 상품 수량 */
+    private int quantity;
+
+    /** 상품 금액 */
+    private BigDecimal price;
+}
