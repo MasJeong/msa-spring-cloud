ade754b3af351cf8214f664feee6b9551917a7c6
jeongjihun <jihun3654@naver.com>
2025-04-26 17:31:45 +0900

    chore: :wrench: Update KafkaConsumerConfig to use environment properties
    Replaced hardcoded Kafka configuration values with environment properties for better flexibility and maintainability. Introduced `Environment` injection and added support for dynamically fetching Kafka server, group ID, and offset reset configuration.

--- order-service/src/main/java/com/example/orderservice/com/config/KafkaProducerConfig.java
+++ order-service/src/main/java/com/example/orderservice/com/config/KafkaProducerConfig.java
@@ -2,6 +2,7 @@ package com.example.orderservice.com.config;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -16,6 +17,9 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String bootstrapServers;
+
     /**
      * Kafka 프로듀서 팩토리 생성 Bean
      *
@@ -26,7 +30,8 @@ public class KafkaProducerConfig {
         Map<String, Object> properties = new HashMap<>();
 
         // Kafka 브로커의 주소 설정
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9094");
+//        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9094");
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
